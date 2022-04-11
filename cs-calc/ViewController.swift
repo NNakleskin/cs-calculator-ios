e6d38944ce5106795595a4a9a80a00a68954759c
Nikita Nakleskin <nakleskin.nv@gmail.com>
2022-04-11 03:52:31 +0300

    Added fact and delete buttons support, added Int8 and Int32 convert to binary support

--- cs-calc/ViewController.swift
+++ cs-calc/ViewController.swift
@@ -20,6 +20,15 @@ extension BinaryInteger {
     }
 }
 
+func fact(x:Int) -> Int
+{
+    if x == 1
+    {
+        return x;
+    }
+    return x * fact(x: x - 1);
+}
+
 class ViewController: UIViewController
 {
     var UserNum1: Double = 0;
@@ -42,11 +51,11 @@ class ViewController: UIViewController
     
     @IBAction func ButtonActions(_ sender: UIButton)
     {
-        if result.text != ""
+        if result.text != "" && sender.tag != 16
         {
             if sender.tag == 10 // bin
             {
-                if UserNum1 < 256
+                if UserNum1 < 256 && UserNum1 >= 0
                 {
                     result.text = String(UInt8(Int(UserNum1)).binaryDescription  )
                 }
@@ -55,29 +64,42 @@ class ViewController: UIViewController
                     result.text = "UInt8 overflow"
                 
                 }
-                if UserNum1 < 4294967295
+                if UserNum1 < 4294967295 && UserNum1 >= 0
                 {
-                    resultSmall.text = String(UInt32(Int(UserNum1)).binaryDescription  )
+                    resultSmall.text = String(UInt32(Int(UserNum1)).binaryDescription)
                 }
                 else
                 {
                     resultSmall.text = "UInt32 overflow"
+                }
+                if UserNum1 < 0 // Add overflow errors
+                {
+                    result.text = String(Int(Int(UserNum1)).binaryDescription)
+                    resultSmall.text = String(Int32(Int(UserNum1)).binaryDescription)
                 }
             }
             else if sender.tag == 13 // xor
             {
                 resultSmall.text = ""
                 result.text = "0x" + String(Int(UserNum1), radix: 16)
-                
-                
             }
             else if sender.tag == 15 // not
             {
+                result.text = " "
                 result.text = String((~Int(UserNum1)));
-                
-                
+            }
+            else if sender.tag == 24 // fact
+            {
+                result.text = " "
+                result.text = String((fact(x:Int(UserNum1))));
             }
 
+        }
+        else if sender.tag == 16 // delete
+        {
+            UserNum1 = 0.0
+            UserNum2 = 0.0
+            result.text = "";
         }
         
     }
